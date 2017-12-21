f6b89791db1773a290adaa6ebc6cece935c1e207
Alexandre Mendes <alexandregme@gmail.com>
2017-12-21 18:45:02 -0200

    [server] allowing CORS

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express'),
     app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(express.static('www'));
 app.set('port', process.env.PORT || 8100);
 app.listen(app.get('port'), function () {
